becdbe3f5f181469cdf0d65f68173ccddd46f0cc
dwaynediddy <burrows.dwayne@icloud.com>
2021-03-03 12:29:28 +0100

    products from commercejs coming through but without pics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import { commerce } from './components/lib/commerce'
 import Products from './components/Products'
 
 
 function App() {
+  const [ products, setProdcuts ] = useState([])
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list()
+
+    setProdcuts(data)
+  }
+
+  useEffect(() => fetchProducts(), [])
+
   return (
     <div className="App">
-      <Products />
+      <Products products={products} />
     </div>
   );
 }
